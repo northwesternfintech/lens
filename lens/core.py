b8cb23cf01da1dcc0c3f2d2194151fe6007ca042
Ethan Havemann <echavemann@gmail.com>
2025-06-10 14:55:17 -0500

    >

--- lens/core.py
+++ lens/core.py
@@ -1,14 +1,17 @@
 from pathlib import Path
 
+import pandas as pd
 from loguru import logger
 
+from lens.descriptor import DataDescriptor, load_data_descriptor
 from lens.util.hostname import check_valid_lens_hostname, get_hostname
 from lens.util.paths import check_file_structure_correct, check_metadata_exists, check_metadata_version_match
 
 
 class Lens:
     def __init__(self) -> None:
-        pass
+        self.started = False
+        self.descriptor: DataDescriptor
 
     def startup(self, data_root: Path) -> None:
         if not check_valid_lens_hostname(get_hostname()):
@@ -24,3 +27,30 @@ class Lens:
             raise RuntimeError("File structure does not match expected directory structure.")
         logger.info("File structure is correct.")
         logger.info("Lens startup succeeded.")
+        self.started = True
+        self.descriptor = load_data_descriptor(data_root)
+
+    def load_trades_for_day(self, coin: str, date: str) -> pd.DataFrame:
+        self._ensure_started()
+        if not self._check_coin_available(coin):
+            raise RuntimeError("Coin is not available in this dataset.")
+        for file in self.descriptor.data_paths[coin].trade_paths:
+            if date + ".csv" == file.name:
+                return pd.read_csv(file)
+        raise RuntimeError("Failed to find requested date.")
+
+    def load_updates_for_day(self, coin: str, date: str) -> pd.DataFrame:
+        self._ensure_started()
+        if not self._check_coin_available(coin):
+            raise RuntimeError("Coin is not available in this dataset.")
+        for file in self.descriptor.data_paths[coin].depth_paths:
+            if date + ".csv" == file.name:
+                return pd.read_csv(file)
+        raise RuntimeError("Failed to find requested date.")
+
+    def _check_coin_available(self, coin: str) -> bool:
+        return coin in self.descriptor.data_paths.keys()
+
+    def _ensure_started(self) -> None:
+        if not self.started:
+            raise RuntimeError("This operation requires lens to be started before calling.")
