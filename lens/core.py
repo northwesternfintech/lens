aeb84bb892b8d48058a0f5142ee35d3a6be8261a
Ethan Havemann <echavemann@gmail.com>
2025-06-10 15:18:02 -0500

    testing core

--- lens/core.py
+++ lens/core.py
@@ -14,18 +14,9 @@ class Lens:
         self.descriptor: DataDescriptor
 
     def startup(self, data_root: Path) -> None:
-        if not check_valid_lens_hostname(get_hostname()):
-            raise RuntimeError("Cannot start lens on invalid host.")
-        logger.info("Host check succeeded.")
-        if not check_metadata_exists(data_root):
-            raise RuntimeError("Failed to find metadata.json file in the data directory.")
-        logger.info("Metadata found.")
-        if not check_metadata_version_match(data_root):
-            raise RuntimeError("Metadata version does not match current version!")
-        logger.info("Metadata versions match.")
-        if not check_file_structure_correct(data_root):
-            raise RuntimeError("File structure does not match expected directory structure.")
-        logger.info("File structure is correct.")
+        self._startup_check_hostname()
+        self._startup_check_metadata(data_root)
+        self._startup_check_files(data_root)
         logger.info("Lens startup succeeded.")
         self.started = True
         self.descriptor = load_data_descriptor(data_root)
@@ -36,16 +27,16 @@ class Lens:
             raise RuntimeError("Coin is not available in this dataset.")
         for file in self.descriptor.data_paths[coin].trade_paths:
             if date + ".csv" == file.name:
-                return pd.read_csv(file)
+                return pd.read_csv(file, sep="|")
         raise RuntimeError("Failed to find requested date.")
 
-    def load_updates_for_day(self, coin: str, date: str) -> pd.DataFrame:
+    def load_depth_for_day(self, coin: str, date: str) -> pd.DataFrame:
         self._ensure_started()
         if not self._check_coin_available(coin):
             raise RuntimeError("Coin is not available in this dataset.")
         for file in self.descriptor.data_paths[coin].depth_paths:
             if date + ".csv" == file.name:
-                return pd.read_csv(file)
+                return pd.read_csv(file, sep='|')
         raise RuntimeError("Failed to find requested date.")
 
     def _check_coin_available(self, coin: str) -> bool:
@@ -54,3 +45,26 @@ class Lens:
     def _ensure_started(self) -> None:
         if not self.started:
             raise RuntimeError("This operation requires lens to be started before calling.")
+
+    def _startup_check_hostname(self) -> None:
+        if not check_valid_lens_hostname(get_hostname()):
+            raise RuntimeError("Cannot start lens on invalid host.")
+        logger.info("Host check succeeded.")
+
+    def _startup_check_metadata(self, data_root: Path) -> None:
+        if not check_metadata_exists(data_root):
+            raise RuntimeError("Failed to find metadata.json file in the data directory.")
+        logger.info("Metadata found.")
+        if not check_metadata_version_match(data_root):
+            raise RuntimeError("Metadata version does not match current version!")
+        logger.info("Metadata versions match.")
+
+    def _startup_check_files(self, data_root: Path) -> None:
+        if not check_file_structure_correct(data_root):
+            raise RuntimeError("File structure does not match expected directory structure.")
+        logger.info("File structure is correct.")
+
+
+    def _force_startup_unsafe(self, data_root: Path) -> None:
+        self.started = True
+        self.descriptor = load_data_descriptor(data_root)
---
+++ lens/test/test_core.py
@@ -0,0 +1,48 @@
+
+import pytest
+from lens.core import Lens
+from lens.test.common import TEST_DATA_PATH
+
+
+def test_core_startup_metadata() -> None:
+    ln = Lens()
+    ln._startup_check_metadata(TEST_DATA_PATH)
+
+def test_core_startup_file_structure() -> None:
+    ln = Lens()
+    ln._startup_check_files(TEST_DATA_PATH)
+
+def test_core_startup_trade_load() -> None:
+    ln = Lens()
+    ln._force_startup_unsafe(TEST_DATA_PATH)
+    df = ln.load_trades_for_day("avax", "2025-02-21")
+    assert df.shape[1] == 5
+
+def test_core_startup_trade_load_invalid_symbol() -> None:
+    ln = Lens()
+    ln._force_startup_unsafe(TEST_DATA_PATH)
+    with pytest.raises(RuntimeError):
+        ln.load_trades_for_day("avaxnop", "2025-02-21")
+
+def test_core_startup_trade_load_nonexistent_date() -> None:
+    ln = Lens()
+    ln._force_startup_unsafe(TEST_DATA_PATH)
+    with pytest.raises(RuntimeError):
+        ln.load_trades_for_day("avax", "2025-02-28")
+
+def test_core_startup_depth() -> None:
+    ln = Lens()
+    ln._force_startup_unsafe(TEST_DATA_PATH)
+    ln.load_depth_for_day("avax", "2025-02-21")
+
+def test_core_startup_depth_load_invalid_symbol() -> None:
+    ln = Lens()
+    ln._force_startup_unsafe(TEST_DATA_PATH)
+    with pytest.raises(RuntimeError):
+        ln.load_trades_for_day("avaxnop", "2025-02-21")
+#
+def test_core_startup_depth_load_nonexistent_date() -> None:
+    ln = Lens()
+    ln._force_startup_unsafe(TEST_DATA_PATH)
+    with pytest.raises(RuntimeError):
+        ln.load_trades_for_day("avax", "2025-02-28")
